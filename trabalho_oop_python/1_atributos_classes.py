f421dc6ea7474af6cbe76d4b2f3ecb456a98bfee
Gustavo Henrique <69666481+DjdogeGamer@users.noreply.github.com>
2022-03-19 02:31:31 -0300

    Add files via upload

---
+++ trabalho_oop_python/1_atributos_classes.py
@@ -0,0 +1,14 @@
+
+# Assim como na vida real, objetos possuem suas próprias características, podemos chamar essas características de atributos.
+
+class Carro():
+    pass
+
+c1 = Carro()
+c1.marca = "Renault"
+c1.modelo = "Kwid"
+c1.cor = "Laranja"
+
+print(c1.marca)
+print(c1.modelo)
+print(c1.cor)
