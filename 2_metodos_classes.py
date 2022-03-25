7238d87452ffd227affa81470b40f3dad8467055
Gustavo Henrique <69666481+DjdogeGamer@users.noreply.github.com>
2022-03-25 16:55:13 -0300

    Update 2_metodos_classes.py

--- 2_metodos_classes.py
+++ 2_metodos_classes.py
@@ -5,8 +5,8 @@ class Carro():
     def buzinar(self): 
         print("Bii Bii!")
 
-# O argumento especial self indica "ele mesmo (objeto)", o que garante que as intâncias serão associadas exclusivamente à este objeto
-# self é equivalente ao método ".this" "esse" em java por exemplo
+# O argumento especial self indica "ele mesmo (objeto)", o que garante que as intâncias serão associadas exclusivamente ao objeto que foi chamado
+# self também equivalente ao método ".this" "esse" em java por exemplo
 
 c1 = Carro()
 c1.buzinar()
